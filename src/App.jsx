04a7fa475a52e154d36ca102f5787a74a401919a
Juan Carlos Ibarra Velásquez <juan.carlos.ibarra@clases.edu.sv>
2026-07-26 22:50:02 -0600

    Agrega codigo fuente: React, Vite y Docker

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  return (
+    <main className="contenedor-principal">
+      <h1>Catálogo de Videojuegos</h1>
+      <p>Aplicación  con React, Vite y Docker.</p>
+    </main>
+  )
+}
+
+export default App
